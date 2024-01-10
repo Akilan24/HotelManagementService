72dbda380ca1c4ce1a5d8d758525274e4c654642
Akilan24 <akilan24012001@gmail.com>
2024-01-10 16:39:09 +0530

    mail

--- Mail-service/src/main/java/com/mailservice/MailServiceImpl.java
+++ Mail-service/src/main/java/com/mailservice/MailServiceImpl.java
@@ -3,6 +3,10 @@ package com.mailservice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +15,28 @@ public class MailServiceImpl implements MailService {
 	@Autowired
 	MailRepository mailrepo;
 
+	@Autowired
+	private JavaMailSender mailSender;
+	@Autowired
+	ApplicationEventPublisher aep;
+
 	@Override
 	public String sendMail(Mail mail) {
 		Mail m = new Mail(mail.getToEmail(), mail.getSubject(), mail.getBody());
+		aep.publishEvent(m);
 		mailrepo.save(m);
 		return "Mail sent Succesfully";
+	}
+
+	@EventListener
+	public void mailsender(Mail m) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("fromemail@gmail.com");
+		message.setTo(m.getToEmail());
+		message.setText(m.getBody());
+		message.setSubject(m.getSubject());
+		mailSender.send(message);
+		
 	}
 
 	@Override
